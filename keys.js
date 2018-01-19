39ac28d2b388c22cc78542b2c2a94d9fdcf511f4
nworford <nathanworford@gmail.com>
2018-01-19 13:18:19 -0700

    Update keys.js

--- keys.js
+++ keys.js
@@ -7,4 +7,11 @@ var twitterKeys = {
   access_token_secret: 'Z1AXv8lPA8JkcsbZHnoDSsfz7BvDFh2jW5nBgvI28D4Hm',
 }
 
+var spotifyKeys = {
+  client_id: 'HnIOp6wTrwoPacTfA89Rq00Jj',
+  client_secret: 'GffbG25sVYIdFNd4xY413sZtsjvWEzON21h8pRaF1ud0luWeGE',
+  
+}
+
+module.exports = spotifyKeys
 module.exports = twitterKeys;
