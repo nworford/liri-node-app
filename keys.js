f6d7632e2e4c557b601d1dfdd76b587771c8ccb4
nworford <nathanworford@gmail.com>
2018-01-19 13:07:54 -0700

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'HnIOp6wTrwoPacTfA89Rq00Jj',
+  consumer_secret: 'GffbG25sVYIdFNd4xY413sZtsjvWEzON21h8pRaF1ud0luWeGE',
+  access_token_key: '935980945902940160-uGK7w29T2re6v1vyeS5D7epxy9WDFpX',
+  access_token_secret: 'Z1AXv8lPA8JkcsbZHnoDSsfz7BvDFh2jW5nBgvI28D4Hm',
+}
+
+module.exports = twitterKeys;
