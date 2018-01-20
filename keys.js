fcc028981eeffe3dc1c28b5cf6914f3109460336
nlw113 <nathanworford@gmail.com>
2018-01-20 11:59:21 -0700

    update

--- keys.js
+++ keys.js
@@ -1,11 +1,19 @@
 console.log('this is loaded');
 
+
 var twitterKeys = {
   consumer_key: 'HnIOp6wTrwoPacTfA89Rq00Jj',
   consumer_secret: 'GffbG25sVYIdFNd4xY413sZtsjvWEzON21h8pRaF1ud0luWeGE',
   access_token_key: '935980945902940160-uGK7w29T2re6v1vyeS5D7epxy9WDFpX',
   access_token_secret: 'Z1AXv8lPA8JkcsbZHnoDSsfz7BvDFh2jW5nBgvI28D4Hm',
 }
+
+// var twitterKeys = {
+//   consumer_key: process.env.HnIOp6wTrwoPacTfA89Rq00Jj,
+//   consumer_secret: process.env.GffbG25sVYIdFNd4xY413sZtsjvWEzON21h8pRaF1ud0luWeGE,
+//   access_token_key: process.env.935980945902940160-uGK7w29T2re6v1vyeS5D7epxy9WDFpX,
+//   access_token_secret: process.env.Z1AXv8lPA8JkcsbZHnoDSsfz7BvDFh2jW5nBgvI28D4Hm,
+// }
 
 var spotifyKeys = {
   client_id: 'HnIOp6wTrwoPacTfA89Rq00Jj',
@@ -17,5 +25,11 @@ var omdbKey = {
   key: '87c37879'
 }
 
-module.exports = spotifyKeys
-module.exports = twitterKeys;
+
+
+
+module.exports = {
+  twitterKeys,
+  spotifyKeys,
+  omdbKey
+};
--- liri.js
+++ liri.js
@@ -1,4 +1,20 @@
 // //At the top of the liri.js file, write the code you need to grab the data from keys.js. Then store the keys in a variable.
+
+var keys = require('./keys');
+let twitterKeys = require('./keys').twitterKeys;
+let spotifyKeys = require('./keys').spotifyKeys;
+let omdbKey = require('./keys').omdbKey;
+
+console.log(keys.twitterKeys);
+console.log(keys.spotifyKeys);
+console.log(keys.omdbKey);
+
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
+
+var client = new Twitter(twitterKeys);
+ 
 // Make it so liri.js can take in one of the following commands:
 
 // my-tweets
@@ -7,11 +23,18 @@
 // do-what-it-says
 
 // node liri.js my-tweets
-
 // This will show your last 20 tweets and when they were created at in your terminal/bash window.
+var nodeArgs = process.argv[2];
+
+if (nodeArgs === 'my-tweets') {
+ client.get('statuses/user_timeline', function(error, tweets, response) {
+    if(error) throw error;
+    
+    console.log(response);  // Raw response object. 
+  });
+}
 
 // node liri.js spotify-this-song '<song name here>'
-
 // This will show the following information about the song in your terminal/bash window
 
 // Artist(s)
