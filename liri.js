f48535ae40734458e836e22e7bd30bca88f04246
nlw113 <nathanworford@gmail.com>
2018-01-31 16:57:20 -0700

    new commit

--- liri.js
+++ liri.js
@@ -26,7 +26,7 @@ var command = process.argv[2];
 
 if (command === 'my-tweets') {
 
-  var params = {screen_name: 'nfl'};
+  var params = {screen_name: 'nathanworford'};
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
       console.log("Tweets");
@@ -56,6 +56,23 @@ if (command === 'spotify-this-song') {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
+
+    else() {
+      var songInfo = data.tracks.items;
+      for (var i = 0; i < 5; i++) {
+        if (songInfo[i] != undefined) {
+            var spotifyResults =
+            "Artist: " + songInfo[i].artists[0].name + "\r\n" +
+            "Song: " + songInfo[i].name + "\r\n" +
+            "Album the song is from: " +songInfo[i].album.name + "\r\n" +
+            "Preview Url: " + songInfo[i].preview_url + "\r\n" +
+            "---------------------" + i + "-----------------------" + "\r\n"
+            console.log(spotifyResults);
+
+        }
+      }
+      
+    }
     console.log("spotify-this-song");
     console.log(data);
   }
@@ -66,8 +83,6 @@ if (command === 'spotify-this-song') {
 // // A preview link of the song from Spotify
 // // The album that the song is from
 
-// // Client ID 22d67a4af3ea4982b12c9c26fd556066
-// // Client Secret acd184d190bd419db59de23938e185fc
 
 // // If no song is provided then your program will default to "The Sign" by Ace of Base.
 
@@ -83,5 +98,54 @@ if (command === 'spotify-this-song') {
 // //    * Language of the movie.
 // //    * Plot of the movie.
 // //    * Actors in the movie.
-   
+ // Movie function, uses the Request module to call the OMDB api
+ function movieThis(){
+  var movie = process.argv[3];
+  if(!movie){
+    movie = "mr nobody";
+  }
+  params = movie
+  request("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var movieObject = JSON.parse(body);
+      //console.log(movieObject); // Show the text in the terminal
+      var movieResults =
+      "------------------------------ begin ------------------------------" + "\r\n"
+      "Title: " + movieObject.Title+"\r\n"+
+      "Year: " + movieObject.Year+"\r\n"+
+      "Imdb Rating: " + movieObject.imdbRating+"\r\n"+
+      "Country: " + movieObject.Country+"\r\n"+
+      "Language: " + movieObject.Language+"\r\n"+
+      "Plot: " + movieObject.Plot+"\r\n"+
+      "Actors: " + movieObject.Actors+"\r\n"+
+      "Rotten Tomatoes Rating: " + movieObject.tomatoRating+"\r\n"+
+      "Rotten Tomatoes URL: " + movieObject.tomatoURL + "\r\n" + 
+      "------------------------------ fin ------------------------------" + "\r\n";
+      console.log(movieResults);
+      log(movieResults); // calling log function
+    } else {
+      console.log("Error :"+ error);
+      return;
+    }
+  });
+};  
 // //    If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'//
+
+function doWhatItSays() {
+  fs.readFile("random.txt", "utf8", function(error, data){
+    if (!error) {
+      doWhatItSaysResults = data.split(",");
+      spotifyThisSong(doWhatItSaysResults[0], doWhatItSaysResults[1]);
+    } else {
+      console.log("Error occurred" + error);
+    }
+  });
+};
+// Do What It Says function, uses the reads and writes module to access the log.txt file and write everything that returns in terminal in the log.txt file
+function log(logResults) {
+  fs.appendFile("log.txt", logResults, (error) => {
+    if(error) {
+      throw error;
+    }
+  });
+}
